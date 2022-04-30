d181da94cd0fcd14af08fd114881bc69e40bded7
Isar <isar.arnautovic16@gmail.com>
2022-04-30 12:44:40 +0200

    Round play function is created

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ function playerChoice() {
 function computerPlay() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
+
+// A round play function is created
+function roundPlay(playerSelection, computerSelection) {
+    playerSelection = playerChoice();
+    computerSelection = computerPlay();
+
+    if (playerSelection === computerSelection) {
+        console.log(`You selected: ${playerSelection}.` + ` The computer selected: ${computerSelection}.` + ` Tie!`);
+        return;
+
+    } else if (playerSelection === `rock` && computerSelection === `scissors` ||
+        playerSelection === `paper` && computerSelection === `rock` ||
+        playerSelection === `scissors` && computerSelection === `paper`) {
+        console.log(`You selected: ${playerSelection}.` + ` The computer selected: ${computerSelection}.` + ` Score goes to player!`);
+        playerScore += 1;
+        return;
+
+    } else if (playerSelection === `scissors` && computerSelection === `rock` ||
+        playerSelection === `rock` && computerSelection === `paper` ||
+        playerSelection === `paper` && computerSelection === `scissors`) {
+        console.log(`You selected: ${playerSelection}.` + ` The computer selected: ${computerSelection}.` + ` Score goes to computer!`);
+        computerScore += 1;
+        return;
+
+    } else {
+        console.log(`You have to pick between rock, paper and scissors!`);
+    }
+}
